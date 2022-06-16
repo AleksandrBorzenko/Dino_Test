613b32d5462b8807b7c3cadad843b9a7296e5017
AleksandrBorzenko <santec777@gmail.com>
2022-06-17 00:31:38 +0700

    Touch screen added

--- Test_Dino/Assets/Scripts/Game/GameController.cs
+++ Test_Dino/Assets/Scripts/Game/GameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,22 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] private WaypointsHolder _waypointsHolder;
     [SerializeField] private Player _player;
+    [SerializeField] private TouchScreen _touchScreen;
 
     private void Start()
     {
         _waypointsHolder.InitializePointsOnScene();
         _player.TakeWaypoints(_waypointsHolder.waypoints);
+        _touchScreen.GameStarted.AddListener(StartGame);
+    }
+
+    private void StartGame()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnDestroy()
+    {
+        _touchScreen.GameStarted.RemoveListener(StartGame);
     }
 }
