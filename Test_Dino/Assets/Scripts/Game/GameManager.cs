cbc9cd585f25e79f8b60a01421dccf1e2f883dc7
AleksandrBorzenko <santec777@gmail.com>
2022-06-16 17:22:23 +0700

    Waypoint holder class created

--- Test_Dino/Assets/Scripts/Game/GameManager.cs
+++ Test_Dino/Assets/Scripts/Game/GameManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Main controller of game process
+/// </summary>
 public class GameManager : MonoBehaviour
 {
     
---
+++ Test_Dino/Assets/Scripts/Game/WaypointsHolder.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointsHolder : MonoBehaviour
+{
+    /// <summary>
+    /// List of waypoints on scene
+    /// </summary>
+    public List<Waypoint> waypoints { get; private set; } = new List<Waypoint>();
+    /// <summary>
+    /// Initialize all waypoints on scene
+    /// </summary>
+    public void InitializePointsOnScene()
+    {
+        foreach (Transform child in transform)
+        {
+            var waypoint = child.GetComponent<Waypoint>();
+            waypoint.InitializeWaypoint();
+            waypoints.Add(waypoint);
+
+        }
+    }
+}
