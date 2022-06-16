8c91a1cdbaebd6c92859acbc17056f099c9d6bcf
AleksandrBorzenko <santec777@gmail.com>
2022-06-17 03:08:59 +0700

    Adding functionality to bullet

--- Test_Dino/Assets/Scripts/Bullet/Bullet.cs
+++ Test_Dino/Assets/Scripts/Bullet/Bullet.cs
@@ -6,5 +6,36 @@ using UnityEngine;
 /// </summary>
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private float _lifeTime = 3;
+    [SerializeField] private float _speed = 3;
+
+    private float _currentLifeTime;
+
+
+    #region Public Methods
+    /// <summary>
+    /// Sets current lifetime to default
+    /// </summary>
+    public void SetCurrentLifeTimeToMax()
+    {
+        _currentLifeTime = _lifeTime;
+    }
+    /// <summary>
+    /// Bullet translates to point
+    /// </summary>
+    /// <param name="point">Tap input position</param>
+    public void TranslateToPoint(Vector3 point)
+    {
+        transform.Translate(point);
+    }
+
+    #endregion
+
     
+
+    private void Update()
+    {
+        if (_currentLifeTime > 0) _currentLifeTime -= Time.deltaTime;
+        else gameObject.SetActive(false);
+    }
 }
--- Test_Dino/Assets/Scripts/Bullet/BulletPool.cs
+++ Test_Dino/Assets/Scripts/Bullet/BulletPool.cs
@@ -10,15 +10,26 @@ public class BulletPool : MonoBehaviour
     [SerializeField] private int _poolCapacity;
 
     private Pool<Bullet> _bulletPool;
+    /// <summary>
+    /// Initializing a bullet pool
+    /// </summary>
+
+    #region Public Methods
 
     public void InitializePool()
     {
-        _bulletPool = new Pool<Bullet>(_bulletPrefab,_poolCapacity,transform);
+        _bulletPool = new Pool<Bullet>(_bulletPrefab, _poolCapacity, transform);
     }
-
+    /// <summary>
+    /// Create bullet from pool in specified position
+    /// </summary>
+    /// <param name="position">Specified position</param>
     public void CreateBullet(Vector3 position)
     {
         var bullet = _bulletPool.GetFreeElement();
         bullet.transform.position = position;
+        bullet.SetCurrentLifeTimeToMax();
     }
+
+    #endregion
 }
