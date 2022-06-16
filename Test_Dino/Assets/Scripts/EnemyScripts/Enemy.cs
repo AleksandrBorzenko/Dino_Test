9d64cf9407937c55c7bb676710d4a8fc60941a14
AleksandrBorzenko <santec777@gmail.com>
2022-06-17 01:07:12 +0700

    Enemies can collide detection

--- Test_Dino/Assets/Scripts/EnemyScripts/Enemy.cs
+++ Test_Dino/Assets/Scripts/EnemyScripts/Enemy.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Enemy facade
+/// </summary>
 public class Enemy : MonoBehaviour
 {
-    
+    private PlayerAnimator _playerAnimator;
+
+    private void Start()
+    {
+        _playerAnimator = new PlayerAnimator(GetComponent<Animator>());
+        _playerAnimator.IdleAnim();
+    }
+
+    private void OnCollisionEnter(Collider collider)
+    {
+        if (collider.TryGetComponent(out Bullet bullet)) 
+        {
+            Destroy(gameObject);
+        }
+    }
 }
