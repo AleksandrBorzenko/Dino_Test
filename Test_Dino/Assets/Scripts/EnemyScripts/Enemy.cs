0a54ee27f4adc4071310d3af8053b9ffdaae68cb
AleksandrBorzenko <santec777@gmail.com>
2022-06-18 01:05:22 +0700

    IVital interface added and implemented to enemy

--- Test_Dino/Assets/Scripts/EnemyScripts/Enemy.cs
+++ Test_Dino/Assets/Scripts/EnemyScripts/Enemy.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 /// <summary>
 /// Enemy facade
 /// </summary>
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour, IVital
 {
+    private const int _maxHealth = 5;
+    private const int _minHealth = 1;
+
     /// <summary>
     /// Enemy body component
     /// </summary>
     public EnemyBody enemyBody { get; private set; }
+    /// <summary>
+    /// The health points of enemy
+    /// </summary>
+    public int health { get; private set; }
 
     private PlayerAnimator _playerAnimator;
     private void Awake()
@@ -17,6 +24,7 @@ public class Enemy : MonoBehaviour
         _playerAnimator = new PlayerAnimator(GetComponent<Animator>());
         _playerAnimator.IdleAnim();
         enemyBody = GetComponentInChildren<EnemyBody>();
+        health = Random.Range(_minHealth, _maxHealth);
     }
 
 }
---
+++ Test_Dino/Assets/Scripts/EnemyScripts/IVital.cs
@@ -0,0 +1,4 @@
+public interface IVital
+{
+    int health { get; }
+}
